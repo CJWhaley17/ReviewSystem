34b969928716d5cf99d704a7e8cfdb8ebc73501e
cjwhaley17 <cwhaley299@gmail.com>
2017-08-04 15:21:03 -0400

    I created some models, but its been a hot second and Im going to need to refactor for sure

---
+++ RatingReviewSystem/RatingReviewSystem.Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingReviewSystem.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public OrganizationRole Role { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ RatingReviewSystem/RatingReviewSystem.Models/OrganizationLeadership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingReviewSystem.Models
+{
+    public class OrganizationLeadership
+    {
+        public Ambassador Ambassador { get; set; }
+        public VPMembership VPMembership { get; set; }
+        public VPMedia VPMedia { get; set; }
+        public EventCoordinator1 EventCoordinator1 { get; set; }
+        public EventCoordinator2 EventCoordinator2 { get; set; }
+        public Treasurer Treasurer { get; set; }
+        public Secretary Secretary { get; set; }
+        public string College { get; set; }
+    }
+}
