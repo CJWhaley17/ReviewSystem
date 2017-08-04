4da9b553799e85fa259b5be7b092e198e444f6b0
cjwhaley17 <cwhaley299@gmail.com>
2017-08-04 14:42:11 -0400

    init setup of the solution

---
+++ RatingReviewSystem/RatingReviewSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RatingReviewSystem.Startup))]
+namespace RatingReviewSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
