6373a31805c4ca9457925a055ff916ff861a1d99
cjwhaley17 <cwhaley299@gmail.com>
2017-08-04 14:50:21 -0400

    added an enum for orgrole

---
+++ RatingReviewSystem/RatingReviewSystem.Models/OrganizationRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingReviewSystem.Models
+{
+    public enum OrganizationRole
+    {
+        Ambassador,
+        VP_Membership,
+        VP_Media,
+        Event_Coordinator1,
+        Event_Coordinator2,
+        Treasurer,
+        Secretary
+    }
+}
